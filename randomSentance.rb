57a528f6d6222dd3bbf8f28195bd06fc6238eed0
Joe Luquette <jtrix6@gmail.com>
2012-04-22 08:30:53 -0400

    renamed 'worpus' stuff

--- randomSentance.rb
+++ randomSentance.rb
@@ -3,8 +3,8 @@ require './util'
 
 model = MultigramModel.new
 
-worpusDir = "samples"
-Dir.chdir(worpusDir)
+sampleDir = "samples"
+Dir.chdir(sampleDir)
 files = Dir.glob("*.txt")
 files.each do |file|
     model.addFile(file)
@@ -14,4 +14,3 @@ puts ""
 puts "Random sentance:"
 puts model.buildRandSentance
 puts ""
-
